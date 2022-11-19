edf62572c25b198d26bb35234b747870cd58b08f
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2022-11-19 18:06:28 -0500

    Website changes

--- Websites/Paypal/script.js
+++ Websites/Paypal/script.js
@@ -26,21 +26,26 @@ document.querySelectorAll('.signup').forEach(signupBtn => {
 
 // Navigation
 const dropdownItems = document.querySelectorAll('.dropdown-hover')
-dropdownItems.forEach((dropdownItem) => {
-    dropdownItem.addEventListener('mouseover', () => {
-        dropdownItem.lastElementChild.style.cssText = 'opacity:1; visibility:visible';
-        document.querySelector('.navbar-wrapper').style.background = 'linear-gradient(to right, #066399, #2f8fdf, #066399)'
-    })
-    dropdownItem.addEventListener('mouseout', () => {
-        dropdownItem.lastElementChild.style.cssText = 'opacity:0; visibility:hidden';
-        document.querySelector('.navbar-wrapper').style.background = 'none'
-    })
-})
 
-const menuIcon = document.querySelector('.menu')
-const navbar = document.querySelector('.navbar')
-
-menuIcon.addEventListener('click', () => {
-    navbar.classList.toggle('change')
-})
+if(window.innerWidth < 1000) {
+    const menuIcon = document.querySelector('.menu')
+    const navbar = document.querySelector('.navbar')
+    
+    menuIcon.addEventListener('click', () => {
+        navbar.classList.toggle('change')
+    })
+} else {
+    dropdownItems.forEach((dropdownItem) => {
+        dropdownItem.addEventListener('mouseover', () => {
+            dropdownItem.lastElementChild.style.cssText = 'opacity:1; visibility:visible';
+            document.querySelector('.navbar-wrapper').style.background = 'linear-gradient(to right, #066399, #2f8fdf, #066399)'
+            dropdownItem.firstElementChild.firstElementChild.style.transform = 'rotate(180deg)'
+        })
+        dropdownItem.addEventListener('mouseout', () => {
+            dropdownItem.lastElementChild.style.cssText = 'opacity:0; visibility:hidden';
+            document.querySelector('.navbar-wrapper').style.background = 'none'
+            dropdownItem.firstElementChild.firstElementChild.style.transform = 'rotate(0)'
+        })
+    })
+}
 // End of Navigation
